d1853d2d055b1aa6c6929b832bed4ac61ebb6f82
memxnse <memonse.gnaho@epitech.eu>
2023-12-04 22:54:12 +0100

    feat: new css style (video) and Api fetched for football

--- script.js
+++ script.js
@@ -11,70 +11,60 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 3000); // The sidebar will change width after 3 seconds (3000 milliseconds)
 });
 
-// let array = []
+function vid_loader(id, myVideo) {
+  var vid = document.getElementById(myVideo);
+  var div = document.getElementById(id);
+  div.onmouseover = function() {
+    vid.style.display = 'block';
+    vid.play();
+  }
+  div.onmouseout = function() {
+    vid.pause();
+    vid.style.display = 'none';
+  }
+}
 
-// async function fetch_competition() {
-  // const url = 'https://football98.p.rapidapi.com/competitions';
-  // console.log(url);
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     'X-RapidAPI-Key': '2d6f2fa199msh3aeca0e03c58ea8p141364jsn129cec6055bc',
-  //     'X-RapidAPI-Host': 'football98.p.rapidapi.com'
-  //   }
-  // };
+async function fetch_football_competition() {
+  const url = 'https://livescore-sports.p.rapidapi.com/v1/competitions/team-statistics?stat_type=main&competition_id=65&locale=EN';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '2d6f2fa199msh3aeca0e03c58ea8p141364jsn129cec6055bc',
+      'X-RapidAPI-Host': 'livescore-sports.p.rapidapi.com'
+    }
+  };
 
-  // try {
-  //   const response = await fetch(url, options);
-  //   const result = await response.json()
-  //   console.log(result);
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json(); // Convertir la réponse en JSON
+    console.log(result["DATA"]);
 
-  //   array = result.data;
-  //   // findElemAndDisplay()
-  // } catch (error) {
-  //   console.error(error);
-  // }
+    // Insérer les données dans la page HTML
+    document.getElementById('PLE').innerHTML = ("Event: " + result["DATA"]["COMPETITION_NAME"]);
+    document.getElementById('PLL').innerHTML = ("Location: " + result["DATA"]["COMPETITION_DESCRIPTION"]);
+    document.getElementById('PLT').innerHTML = ("Team: " + result["DATA"]["STATISTICS"]["0"]["Tids"]["0"]["TEAM_NAME"]);
+    document.getElementById('PLS').innerHTML = ("Total Players Point List: " + result["DATA"]["STATISTICS"]["0"]["Tids"]["0"]["PLAYER_POINTS_LIST"]["0"]["Ttl"]);
 
-//   const url = 'https://football98.p.rapidapi.com/premierleague/news';
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Key': '2d6f2fa199msh3aeca0e03c58ea8p141364jsn129cec6055bc',
-//       'X-RapidAPI-Host': 'football98.p.rapidapi.com'
-//     }
-//   };
+    document.getElementById('div1').innerHTML = ("Event: " + result["DATA"]["COMPETITION_NAME"]);
+    document.getElementById('div2').innerHTML = ("Location: " + result["DATA"]["COMPETITION_DESCRIPTION"]);
+    document.getElementById('div3').innerHTML = ("Team: " + result["DATA"]["STATISTICS"]["0"]["Tids"]["1"]["TEAM_NAME"]);
+    document.getElementById('div4').innerHTML = ("Total Players Point List: " + result["DATA"]["STATISTICS"]["0"]["Tids"]["1"]["PLAYER_POINTS_LIST"]["0"]["Ttl"]);
 
-//   try {
-//     const response = await fetch(url, options);
-//     const result = await response.text();
-//     console.log(result);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+    document.getElementById('div5').innerHTML = ("Event: " + result["DATA"]["COMPETITION_NAME"]);
+    document.getElementById('div6').innerHTML = ("Location: " + result["DATA"]["COMPETITION_DESCRIPTION"]);
+    document.getElementById('div7').innerHTML = ("Team: " + result["DATA"]["STATISTICS"]["0"]["Tids"]["2"]["TEAM_NAME"]);
+    document.getElementById('div8').innerHTML = ("Total Players Point List: " + result["DATA"]["STATISTICS"]["0"]["Tids"]["2"]["PLAYER_POINTS_LIST"]["0"]["Ttl"]);
 
-// function findElemAndDisplay(name) {
-//   if (array.find(name)) {
-//     console.log("ok");
-//   } else {
-//     console.log("ko");
+    document.getElementById('div9').innerHTML = ("Event: " + result["DATA"]["COMPETITION_NAME"]);
+    document.getElementById('div10').innerHTML = ("Location: " + result["DATA"]["COMPETITION_DESCRIPTION"]);
+    document.getElementById('div11').innerHTML = ("Team: " + result["DATA"]["STATISTICS"]["0"]["Tids"]["3"]["TEAM_NAME"]);
+    document.getElementById('div12').innerHTML = ("Total Players Point List: " + result["DATA"]["STATISTICS"]["0"]["Tids"]["3"]["PLAYER_POINTS_LIST"]["0"]["Ttl"]);
 
-//   }
-
-//   const url = 'https://api-nba-v1.p.rapidapi.com/leagues';
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Key': '2d6f2fa199msh3aeca0e03c58ea8p141364jsn129cec6055bc',
-//       'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-//     }
-//   };
-
-//   try {
-//     const response = await fetch(url, options);
-//     const result = await response.text();
-//     console.log(result);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+    document.getElementById('div13').innerHTML = ("Event: " + result["DATA"]["COMPETITION_NAME"]);
+    document.getElementById('div14').innerHTML = ("Location: " + result["DATA"]["COMPETITION_DESCRIPTION"]);
+    document.getElementById('div15').innerHTML = ("Team: " + result["DATA"]["STATISTICS"]["0"]["Tids"]["4"]["TEAM_NAME"]);
+    document.getElementById('div16').innerHTML = ("Total Players Point List: " + result["DATA"]["STATISTICS"]["0"]["Tids"]["4"]["PLAYER_POINTS_LIST"]["0"]["Ttl"]);
+  } catch (error) {
+    console.error(error);
+  }
+}
